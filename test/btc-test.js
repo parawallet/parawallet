4309b9e8083860048da77e75d9f58cb82b744623
Darkmemo <darkmemo@gmail.com>
2018-01-02 18:28:40 +0300

    Add testing packages; mocha and chai

---
+++ test/btc-test.js
@@ -0,0 +1,9 @@
+import {assert, expect} from 'chai'
+
+import { BtcWallet } from '../src/core/btc-wallet';
+
+describe('BTC Local', () => {
+    it('Create Wallet', () => {
+        let btc = new BtcWallet()        
+    });
+});
