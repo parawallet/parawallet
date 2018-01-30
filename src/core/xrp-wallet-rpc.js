54611c8ce00f773e03825aa34a3bc6e0729f5d88
Cem Karay <cemkaray07@gmail.com>
2018-01-29 22:19:56 -0800

    ripple send payment

--- src/core/xrp-wallet-rpc.js
+++ src/core/xrp-wallet-rpc.js
@@ -4,6 +4,7 @@ const rippleKeyPairs = require("ripple-keypairs");
 const addressCodec = require("ripple-address-codec");
 
 const serverAddress = "wss://s.altnet.rippletest.net:51233";
+
 //const serverAddress = "wss://s1.ripple.com"
 
 export class XrpWalletRpc {
@@ -21,9 +22,9 @@ export class XrpWalletRpc {
             let buffer = Buffer.from(entropy, "hex");
             let entropyBytes = Array.prototype.slice.call(buffer, 0);
             const rippleAddress = api.generateAddress({"entropy": entropyBytes});
-            //this.address = rippleAddress.address;
-            //this.secret = rippleAddress.secret;
-            // comment below to move to real network from testnet
+            this.address = rippleAddress.address;
+            this.secret = rippleAddress.secret;
+            // todo comment below for real network. the below is testnet https://ripple.com/build/xrp-test-net/
             this.address = "rEayHejjpMadHcmSYzSgXWXdTw1qUTp9mY";
             this.secret = "sn4gGtjVgZGJMqF9nK6kdeo1Bu1XD";
             resolve("success");
@@ -43,18 +44,59 @@ export class XrpWalletRpc {
         });
         const promise = api.connect().then(() => {
             /* begin custom code ------------------------------------ */
-            console.log("getting account info for", this.address);
             return api.getAccountInfo(this.address);
         }).then(info => {
             api.disconnect();
-            console.log(info);
             return info.xrpBalance;
         });
         return promise;
     }
 
+    // ADDRESS
+    // r9ZjacYdw46w8jUaT4ApPG2sodRokFXzux
+    // SECRET
+    // sn7DK1JuhzU8MdT6LHmW5AnUePSyU
+    // BALANCE
+    // 10,000 XRP
     send(toAddr, amount, callback) {
-        // todo
+        const toAddress = toAddr;
+//        const toAddress = "r9ZjacYdw46w8jUaT4ApPG2sodRokFXzux";
+        const payment = {
+            "destination": {
+                "address": toAddress,
+                "amount": {
+                    "currency": "XRP",
+                    "value": "" + amount,
+                },
+            },
+            "source": {
+                "address": this.address,
+                "maxAmount": {
+                    "currency": "XRP",
+                    "value": "" + amount,
+                },
+            },
+        };
+
+        try {
+            const api = new RippleAPI({
+                server: serverAddress, // Public test rippled server
+            });
+            const promise = api.connect().then(() => {
+                api.preparePayment(this.address, payment).then(prepared => {
+                        let signedTxn = api.sign(prepared.txJSON, this.secret);
+                        console.log("txn id:" + signedTxn.id);
+                        api.submit(signedTxn.signedTransaction)
+                            .then(result => {
+                                alert(result.resultMessage);
+                            });
+                    },
+                );
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
 }
