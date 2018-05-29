101ce19553a770a1c6fb9f6d6353ea6f9f9a1717
Mustafa Cem Karay <cemkaray07@gmail.com>
2018-05-29 03:44:27 -0700

    make mnemonics optional fixes #32

--- src/ui/pages/wallet-init/create-new-wallet.tsx
+++ src/ui/pages/wallet-init/create-new-wallet.tsx
@@ -1,23 +1,37 @@
 import {toast} from "react-toastify";
 import * as React from "react";
 import {LoginCredentials} from "../../../core/login-credentials";
+import {observer} from "mobx-react";
+import {observable} from "mobx";
+import * as ReactTooltip from "react-tooltip";
 
+@observer
 export class CreateNewWallet extends React.Component<any, any> {
     private appPassInput: HTMLInputElement | null = null;
     private confirmAppPassInput: HTMLInputElement | null = null;
-    private mnemonicPassInput: HTMLInputElement | null = null;
-    private confirmMnemonicPassInput: HTMLInputElement | null = null;
+    private mnemonicPassphraseInput: HTMLInputElement | null = null;
+    private confirmMnemonicPassphraseInput: HTMLInputElement | null = null;
+    private showAdvancedOptionsInput: HTMLInputElement | null = null;
+    @observable
+    private showAdvancedOptions: boolean | null = false;
+
+    private mnemonicPassphraseTip: string = "Mnemonic Passphrase adds a second layer of security to your crypto wallet.\n" +
+        "Both the mnemonic phrase (generated 12 words) and the passphrase are required to recover the wallet.\n" +
+        "If you forget passphrase you will lose all your money and wallet.";
 
     constructor(props: any) {
         super(props);
         this.handle = this.handle.bind(this);
+        this.handleMnemonicPassphraseCheckbox = this.handleMnemonicPassphraseCheckbox.bind(this);
     }
 
     public render() {
         return (
             <div className="login-div w-100 h-100">
-                <div className="text-center w-50" style={{margin: "auto"}}>
+                <div className="w-50" style={{margin: "auto"}}>
                     <img src="images/wallet_logo_inv.png" className="login-logo"/>
+                    <h4>Create New Wallet</h4>
+                    <br/>
                     <div className="form-group">
                         <input type="password" className="form-control form-control-lg"
                                placeholder={"Wallet Password"}
@@ -28,51 +42,75 @@ export class CreateNewWallet extends React.Component<any, any> {
                                placeholder={"Confirm Wallet Password"}
                                ref={(input) => this.confirmAppPassInput = input}/>
                     </div>
-                    <div className="form-group">
-                        <input type="password"
-                               placeholder={"Mnemonic Salt"}
-                               className="form-control form-control-lg" ref={(input) => this.mnemonicPassInput = input}/>
+                    <div className="form-group text-left">
+                        <div className="form-check" data-tip="Not recommended for users new to crypto space.">
+                            <input type="checkbox" id="PassphraseCheckbox"
+                                   className="form-control form-control-lg form-check-input"
+                                   onChange={this.handleMnemonicPassphraseCheckbox}
+                                   ref={(input) => this.showAdvancedOptionsInput = input}/>
+                            <label className="form-check-label" htmlFor="PassphraseCheckbox"
+                                   >Advanced Options</label>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <input type="password" className="form-control form-control-lg"
-                               placeholder={"Confirm Mnemonic Salt"}
-                               ref={(input) => this.confirmMnemonicPassInput = input}/>
-                    </div>
+                    {this.showAdvancedOptions ? (
+                        <div>
+                            <div className="form-group">
+                                <input type="password"
+                                       data-tip={this.mnemonicPassphraseTip}
+                                       placeholder={"Mnemonic Passphrase"}
+                                       className="form-control form-control-lg"
+                                       ref={(input) => this.mnemonicPassphraseInput = input}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control form-control-lg"
+                                       data-tip={this.mnemonicPassphraseTip}
+                                       placeholder={"Confirm Mnemonic Passphrase"}
+                                       ref={(input) => this.confirmMnemonicPassphraseInput = input}/>
+                            </div>
+                        </div>) : ""}
                     <div className="btn-group w-75" role="group">
                         <button
                             className="btn-lg btn-light w-50"
                             onClick={this.props.reset}>
                             <i className="fas fa-arrow-left"/> Back
                         </button>
-                        <button
-                            className="btn-lg btn-light w-50"
-                            onClick={this.handle}>
+                        <button data-tip="Create the wallet!"
+                                className="btn-lg btn-light w-50"
+                                onClick={this.handle}>
                             <i className="fas fa-plus"/> Create
                         </button>
                     </div>
                 </div>
+                <ReactTooltip multiline={true} effect={"solid"} className={"w-50"}/>
             </div>
         );
+    }
+
+    public componentDidUpdate() {
+        ReactTooltip.rebuild();
+    }
+
+    private handleMnemonicPassphraseCheckbox(event: any) {
+        this.showAdvancedOptions = this.showAdvancedOptionsInput && this.showAdvancedOptionsInput.checked;
     }
 
     private handle(event: any) {
         event.preventDefault();
         const appPass = this.appPassInput ? this.appPassInput.value : null;
-        const mnemonicPass = this.mnemonicPassInput ? this.mnemonicPassInput.value : null;
+        const mnemonicPassphrase = this.showAdvancedOptions && this.mnemonicPassphraseInput ? this.mnemonicPassphraseInput.value : "";
 
         const confirmAppPass = this.confirmAppPassInput ? this.confirmAppPassInput.value : "";
-        const confirmMnemonicPass = this.confirmMnemonicPassInput ? this.confirmMnemonicPassInput.value : "";
+        const confirmMnemonicPassphrase = this.confirmMnemonicPassphraseInput ? this.confirmMnemonicPassphraseInput.value : "";
 
         if (!appPass || appPass !== confirmAppPass) {
             toast.error("Wallet passwords do not match!", {autoClose: 5000});
             return;
         }
-        if (mnemonicPass !== confirmMnemonicPass) {
-            toast.error("Mnemonic salts do not match!", {autoClose: 5000});
+        if (this.showAdvancedOptions && mnemonicPassphrase !== confirmMnemonicPassphrase) {
+            toast.error("Mnemonic pass phrases do not match!", {autoClose: 5000});
             return;
         }
-
-        const creds = new LoginCredentials(appPass, mnemonicPass);
+        const creds = new LoginCredentials(appPass, mnemonicPassphrase);
         this.props.handle(creds);
     }
 }
--- src/ui/pages/wallet-init/restore-wallet.tsx
+++ src/ui/pages/wallet-init/restore-wallet.tsx
@@ -2,23 +2,34 @@ import {toast} from "react-toastify";
 import * as React from "react";
 import {LoginCredentials} from "../../../core/login-credentials";
 import * as bip39 from "bip39";
+import {observer} from "mobx-react";
+import {observable} from "mobx";
+import * as ReactTooltip from "react-tooltip";
 
+@observer
 export class RestoreWallet extends React.Component<any, any> {
     private appPassInput: HTMLInputElement | null = null;
     private confirmAppPassInput: HTMLInputElement | null = null;
     private mnemonicInput: HTMLInputElement | null = null;
     private mnemonicPassInput: HTMLInputElement | null = null;
+    private showMnemonicPassInput: HTMLInputElement | null = null;
+    @observable
+    private showMnemonicPass: boolean | null = false;
+
 
     constructor(props: any) {
         super(props);
         this.handle = this.handle.bind(this);
+        this.handleMnemonicPassphraseCheckbox = this.handleMnemonicPassphraseCheckbox.bind(this);
     }
 
     public render() {
         return (
             <div className="login-div w-100 h-100">
-                <div className="text-center w-50" style={{margin: "auto"}}>
+                <div className="w-50" style={{margin: "auto"}}>
                     <img src="images/wallet_logo_inv.png" className="login-logo"/>
+                    <h4>Create New Wallet</h4>
+                    <br/>
                     <div className="form-group">
                         <input type="text" className="form-control form-control-lg"
                                placeholder={"Application Password"}
@@ -34,12 +45,23 @@ export class RestoreWallet extends React.Component<any, any> {
                                placeholder={"Mnemonic"}
                                ref={(input) => this.mnemonicInput = input}/>
                     </div>
+                    <div className="form-group text-left">
+                        <div className="form-check" data-tip="Only needed if you had set a passphrase for mnemonic.">
+                            <input type="checkbox" id="PassphraseCheckbox"
+                                   className="form-control form-control-lg form-check-input"
+                                   onChange={this.handleMnemonicPassphraseCheckbox}
+                                   ref={(input) => this.showMnemonicPassInput = input}/>
+                            <label className="form-check-label" htmlFor="PassphraseCheckbox">
+                            Mnemonic Passphrase</label>
+                        </div>
+                    </div>
+                    {this.showMnemonicPass ? (
                     <div className="form-group">
                         <input type="text" className="form-control form-control-lg"
-                               placeholder={"Mnemonic Salt"}
+                               placeholder={"Mnemonic Passphrase"}
                                ref={(input) => this.mnemonicPassInput = input}/>
                     </div>
-
+                    ) : ""}
                     <div className="btn-group w-75" role="group">
                         <button
                             className="btn-lg btn-light w-50"
@@ -53,8 +75,17 @@ export class RestoreWallet extends React.Component<any, any> {
                         </button>
                     </div>
                 </div>
+                <ReactTooltip />
             </div>
         );
+    }
+
+    private handleMnemonicPassphraseCheckbox(event: any) {
+        this.showMnemonicPass = this.showMnemonicPassInput && this.showMnemonicPassInput.checked;
+    }
+
+    public componentDidUpdate() {
+        ReactTooltip.rebuild();
     }
 
     private handle(event: any) {
@@ -64,14 +95,14 @@ export class RestoreWallet extends React.Component<any, any> {
         const confirmAppPass = this.confirmAppPassInput ? this.confirmAppPassInput.value : null;
 
         const mnemonic = this.mnemonicInput ? this.mnemonicInput.value : "";
-        const mnemonicPass = this.mnemonicPassInput ? this.mnemonicPassInput.value : "";
+        const mnemonicPass = this.showMnemonicPass && this.mnemonicPassInput ? this.mnemonicPassInput.value : "";
 
         if (!appPass || appPass !== confirmAppPass) {
             toast.error("Application passwords do not match!", {autoClose: 5000});
             return;
         }
         if (!bip39.validateMnemonic(mnemonic)) {
-            // TODO: we can allow invalid mnemonics
+            // TODO: we can allow invalid mnemonics  -- enes: why?
             toast.error("Invalid mnemonic!", {autoClose: 5000});
             return;
         }
